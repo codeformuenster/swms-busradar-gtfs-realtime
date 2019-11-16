5a67cb58aabb542d3c49199e169956c8b86f2c44
ubergesundheit <ubergesundheit@googlemail.com>
2019-11-16 16:09:20 +0100

    create static GTFS feed zip with added feed-info.txt in initcontainer

--- initcontainer/fetch-gtfsfeed.sh
+++ initcontainer/fetch-gtfsfeed.sh
@@ -16,3 +16,13 @@ unzip -o /gtfsfeed/feed.zip -d /gtfsfeed
 
 # convert files to unix format
 dos2unix /gtfsfeed/*.txt
+
+# for usage in opentripplanner, create a non standard feed-info.txt
+# See https://github.com/HSLdevcom/OpenTripPlanner-data-container/blob/b14f9b96d968940af7c9d52e57a838bace2ca710/task/setFeedId.js
+cat <<- EOF > /gtfsfeed/feed-info.txt
+feed_publisher_name,feed_publisher_url,feed_lang,feed_id
+Code for Münster,https://swms-busradar-gtfs-realtime.codeformuenster.org/stadtwerke_feed_20191028-with-feed_id.zip,de,STWMS
+EOF
+
+# create zip file
+zip -9 -j /gtfsfeed/stadtwerke_feed_20191028-with-feed_id.zip /gtfsfeed/*.txt
